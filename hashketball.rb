73de428432133f9b77b44620bd09537d813f05d5
Jodi Croft <jodipruett@gmail.com>
2020-05-23 18:09:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,31 @@
-# Write your code here!
+game_hash {
+  :home => {
+    :team_name => "",
+    :colors => [","],
+    :players => [{
+      :player_name => "",
+      :number => ,
+      :shoe => ,
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks 
+      }]
+  }
+  :away => {
+    :team_name => "",
+    :colors => [","],
+    :players => [{
+      :player_name => "",
+      :number => ,
+      :shoe => ,
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks 
+      }]
+}
