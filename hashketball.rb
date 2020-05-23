c754226e57f9e273e39fc3757117cc3a1d9d475e
Jodi Croft <jodipruett@gmail.com>
2020-05-23 18:19:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,19 +1,28 @@
 def game_hash
   {
   :home => {
-    :team_name => "",
-    :colors => [","],
-    :players => [{
-      :player_name => "",
-      :number => ,
-      :shoe => ,
-      :points => ,
-      :rebounds => ,
-      :assists => ,
-      :steals => ,
-      :blocks => ,
-      :slam_dunks
-      }]
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black, White"],
+    :players => [
+      {:player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1},
+      {:player_name => "Reggie Evans",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1},}
+      ]
   }
   :away => {
     :team_name => "",
